f8a9aba144e1b622c5085cfde1dda9dc51a9a120
ahmadsutojo <72114941+ahmadsutojo@users.noreply.github.com>
2020-10-01 11:02:25 +0700

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module GuestNumber {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires javafx.media;
     requires com.jfoenix;
     requires Nodes;
